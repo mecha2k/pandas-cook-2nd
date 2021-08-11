983fcdc4bf7033bfa6631a3c7226c8b5d5a4ac4b
mecha2k <mecha2k@naver.com>
2021-08-11 10:41:56 +0900

    ch07 (Filtering Rows)

--- README.md
+++ README.md
@@ -4,6 +4,23 @@ This is the code repository for [Pandas 1.x Cookbook - 2nd Edition](https://www.
 ## About the Book
 A new edition of the bestselling Pandas cookbook updated to pandas 1.x with new chapters on creating and testing, and exploratory data analysis. Recipes are written with modern pandas constructs. This book also covers EDA, tidying data, pivoting data, time-series calculations, visualizations, and more.
 
+## Contents
+* Chapter 01: Pandas Foundations 1
+* Chapter 02: Essential DataFrame Operations 45
+* Chapter 03: Creating and Persisting DataFrames 81
+* Chapter 04: Beginning Data Analysis 115
+* Chapter 05: Exploratory Data Analysis 139
+* Chapter 06: Selecting Subsets of Data 189
+* Chapter 07: Filtering Rows 209
+* Chapter 08: Index Alignment 245
+* Chapter 09: Grouping for Aggregation, Filtration, and Transformation 285
+* Chapter 10: Restructuring Data into a Tidy Form 349
+* Chapter 11: Combining Pandas Objects 401
+* Chapter 12: Time Series Analysis 429
+* Chapter 13: Visualization with Matplotlib, Pandas, and Seaborn 485
+* Chapter 14: Debugging and Testing Pandas 553
+
+
 ## Instructions and Navigation
 All of the code is organized into folders. Each folder starts with a number followed by the application name. For example, Chapter02.
 
--- src/c07-code.py
+++ src/c07-code.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+from pandas.testing import assert_frame_equal
 from icecream import ic
 
 pd.set_option("max_columns", 4, "max_rows", 10, "max_colwidth", 12)
@@ -7,24 +10,24 @@ pd.set_option("max_columns", 4, "max_rows", 10, "max_colwidth", 12)
 
 if __name__ == "__main__":
     movie = pd.read_csv("data/movie.csv", index_col="movie_title")
-    movie[["duration"]].head()
+    ic(movie[["duration"]].head())
     movie_2_hours = movie["duration"] > 120
-    movie_2_hours.head(10)
-    movie_2_hours.sum()
-    movie_2_hours.mean()
-    movie["duration"].dropna().gt(120).mean()
-    movie_2_hours.describe()
-    movie_2_hours.value_counts(normalize=True)
-    movie_2_hours.astype(int).describe()
+    ic(movie_2_hours.head(10))
+    ic(movie_2_hours.sum())
+    ic(movie_2_hours.mean())
+    ic(movie["duration"].dropna().gt(120).mean())
+    ic(movie_2_hours.describe())
+    ic(movie_2_hours.value_counts(normalize=True))
+    ic(movie_2_hours.astype(int).describe())
     actors = movie[["actor_1_facebook_likes", "actor_2_facebook_likes"]].dropna()
-    (actors["actor_1_facebook_likes"] > actors["actor_2_facebook_likes"]).mean()
+    ic((actors["actor_1_facebook_likes"] > actors["actor_2_facebook_likes"]).mean())
 
     movie = pd.read_csv("data/movie.csv", index_col="movie_title")
     criteria1 = movie.imdb_score > 8
     criteria2 = movie.content_rating == "PG-13"
     criteria3 = (movie.title_year < 2000) | (movie.title_year > 2009)
     criteria_final = criteria1 & criteria2 & criteria3
-    criteria_final.head()
+    ic(criteria_final.head())
 
     movie = pd.read_csv("data/movie.csv", index_col="movie_title")
     crit_a1 = movie.imdb_score > 8
@@ -38,483 +41,140 @@ if __name__ == "__main__":
     final_crit_b = crit_b1 & crit_b2 & crit_b3
 
     final_crit_all = final_crit_a | final_crit_b
-    final_crit_all.head()
+    ic(final_crit_all.head())
+    ic(movie[final_crit_all].head())
+    ic(movie.loc[final_crit_all].head())
+    cols = ["imdb_score", "content_rating", "title_year"]
+    movie_filtered = movie.loc[final_crit_all, cols]
+    ic(movie_filtered.head(10))
 
-    #
-    # # In[23]:
-    #
-    #
-    # movie[final_crit_all].head()
-    #
-    #
-    # # In[24]:
-    #
-    #
-    # movie.loc[final_crit_all].head()
-    #
-    #
-    # # In[25]:
-    #
-    #
-    # cols = ['imdb_score', 'content_rating', 'title_year']
-    # movie_filtered = movie.loc[final_crit_all, cols]
-    # movie_filtered.head(10)
-    #
-    #
-    # # ### How it works...
-    #
-    # # In[26]:
-    #
-    #
-    # movie.iloc[final_crit_all]
-    #
-    #
-    # # In[43]:
-    #
-    #
-    # movie.iloc[final_crit_all.values]
-    #
-    #
-    # # ### There's more...
-    #
-    # # In[44]:
-    #
-    #
-    # final_crit_a2 = ((movie.imdb_score > 8) &
-    #    (movie.content_rating == 'PG-13') &
-    #    ((movie.title_year < 2000) |
-    #     (movie.title_year > 2009))
-    # )
-    # final_crit_a2.equals(final_crit_a)
-    #
-    #
-    # # ## Comparing Row Filtering and Index Filtering
-    #
-    # # ### How to do it...
-    #
-    # # In[45]:
-    #
-    #
-    # college = pd.read_csv('data/college.csv')
-    # college[college['STABBR'] == 'TX'].head()
-    #
-    #
-    # # In[46]:
-    #
-    #
-    # college2 = college.set_index('STABBR')
-    # college2.loc['TX'].head()
-    #
-    #
-    # # In[47]:
-    #
-    #
+    # ic(movie.iloc[final_crit_all])
+    ic(movie.iloc[final_crit_all.values])
+    final_crit_a2 = (
+        (movie.imdb_score > 8)
+        & (movie.content_rating == "PG-13")
+        & ((movie.title_year < 2000) | (movie.title_year > 2009))
+    )
+    ic(final_crit_a2.equals(final_crit_a))
+
+    ## Comparing Row Filtering and Index Filtering
+    college = pd.read_csv("data/college.csv")
+    ic(college[college["STABBR"] == "TX"].head())
+    college2 = college.set_index("STABBR")
+    ic(college2.loc["TX"].head())
     # get_ipython().run_line_magic('timeit', "college[college['STABBR'] == 'TX']")
-    #
-    #
-    # # In[48]:
-    #
-    #
     # get_ipython().run_line_magic('timeit', "college2.loc['TX']")
-    #
-    #
-    # # In[49]:
-    #
-    #
     # get_ipython().run_line_magic('timeit', "college2 = college.set_index('STABBR')")
-    #
-    #
-    # # ### How it works...
-    #
-    # # ### There's more...
-    #
-    # # In[50]:
-    #
-    #
-    # states = ['TX', 'CA', 'NY']
-    # college[college['STABBR'].isin(states)]
-    #
-    #
-    # # In[51]:
-    #
-    #
-    # college2.loc[states]
-    #
-    #
-    # # ## Selecting with unique and sorted indexes
-    #
-    # # ### How to do it...
-    #
-    # # In[52]:
-    #
-    #
-    # college = pd.read_csv('data/college.csv')
-    # college2 = college.set_index('STABBR')
-    # college2.index.is_monotonic
-    #
-    #
-    # # In[53]:
-    #
-    #
-    # college3 = college2.sort_index()
-    # college3.index.is_monotonic
-    #
-    #
-    # # In[54]:
-    #
-    #
+    ic(college[college["STABBR"] == "TX"])
+    ic(college2.loc["TX"])
+    college2 = college.set_index("STABBR")
+    ic(college2)
+
+    states = ["TX", "CA", "NY"]
+    ic(college[college["STABBR"].isin(states)])
+    ic(college2.loc[states])
+
+    ## Selecting with unique and sorted indexes
+    college = pd.read_csv("data/college.csv")
+    college2 = college.set_index("STABBR")
+    ic(college2.index.is_monotonic)
+    college3 = college2.sort_index()
+    ic(college3.index.is_monotonic)
     # get_ipython().run_line_magic('timeit', "college[college['STABBR'] == 'TX']")
-    #
-    #
-    # # In[55]:
-    #
-    #
     # get_ipython().run_line_magic('timeit', "college2.loc['TX']")
-    #
-    #
-    # # In[56]:
-    #
-    #
     # get_ipython().run_line_magic('timeit', "college3.loc['TX']")
-    #
-    #
-    # # In[57]:
-    #
-    #
-    # college_unique = college.set_index('INSTNM')
-    # college_unique.index.is_unique
-    #
-    #
-    # # In[58]:
-    #
-    #
-    # college[college['INSTNM'] == 'Stanford University']
-    #
-    #
-    # # In[59]:
-    #
-    #
-    # college_unique.loc['Stanford University']
-    #
-    #
-    # # In[60]:
-    #
-    #
-    # college_unique.loc[['Stanford University']]
-    #
-    #
-    # # In[61]:
-    #
-    #
+    college_unique = college.set_index("INSTNM")
+    ic(college_unique.index.is_unique)
+    ic(college[college["INSTNM"] == "Stanford University"])
+    ic(college_unique.loc["Stanford University"])
+    ic(college_unique.loc[["Stanford University"]])
     # get_ipython().run_line_magic('timeit', "college[college['INSTNM'] == 'Stanford University']")
-    #
-    #
-    # # In[62]:
-    #
-    #
     # get_ipython().run_line_magic('timeit', "college_unique.loc[['Stanford University']]")
-    #
-    #
-    # # ### How it works...
-    #
-    # # ### There's more...
-    #
-    # # In[63]:
-    #
-    #
-    # college.index = college['CITY'] + ', ' + college['STABBR']
-    # college = college.sort_index()
-    # college.head()
-    #
-    #
-    # # In[64]:
-    #
-    #
-    # college.loc['Miami, FL'].head()
-    #
-    #
-    # # In[65]:
-    #
-    #
+
+    college.index = college["CITY"] + ", " + college["STABBR"]
+    college = college.sort_index()
+    ic(college.head())
+    ic(college.loc["Miami, FL"].head())
     # get_ipython().run_cell_magic('timeit', '', "crit1 = college['CITY'] == 'Miami'\ncrit2 = college['STABBR'] == 'FL'\ncollege[crit1 & crit2]")
-    #
-    #
-    # # In[66]:
-    #
-    #
     # get_ipython().run_line_magic('timeit', "college.loc['Miami, FL']")
-    #
-    #
-    # # ## Translating SQL WHERE clauses
-    #
-    # # ### How to do it...
-    #
-    # # In[67]:
-    #
-    #
-    # employee = pd.read_csv('data/employee.csv')
-    #
-    #
-    # # In[68]:
-    #
-    #
-    # employee.dtypes
-    #
-    #
-    # # In[69]:
-    #
-    #
-    # employee.DEPARTMENT.value_counts().head()
-    #
-    #
-    # # In[70]:
-    #
-    #
-    # employee.GENDER.value_counts()
-    #
-    #
-    # # In[71]:
-    #
-    #
-    # employee.BASE_SALARY.describe()
-    #
-    #
-    # # In[72]:
-    #
-    #
-    # depts = ['Houston Police Department-HPD',
-    #    'Houston Fire Department (HFD)']
-    # criteria_dept = employee.DEPARTMENT.isin(depts)
-    # criteria_gender = employee.GENDER == 'Female'
-    # criteria_sal = ((employee.BASE_SALARY >= 80000) &
-    #    (employee.BASE_SALARY <= 120000))
-    #
-    #
-    # # In[73]:
-    #
-    #
-    # criteria_final = (criteria_dept &
-    #    criteria_gender &
-    #    criteria_sal)
-    #
-    #
-    # # In[74]:
-    #
-    #
-    # select_columns = ['UNIQUE_ID', 'DEPARTMENT',
-    #                   'GENDER', 'BASE_SALARY']
-    # employee.loc[criteria_final, select_columns].head()
-    #
-    #
-    # # ### How it works...
-    #
-    # # ### There's more...
-    #
-    # # In[75]:
-    #
-    #
-    # criteria_sal = employee.BASE_SALARY.between(80_000, 120_000)
-    #
-    #
-    # # In[76]:
-    #
-    #
-    # top_5_depts = employee.DEPARTMENT.value_counts().index[:5]
-    # criteria = ~employee.DEPARTMENT.isin(top_5_depts)
-    # employee[criteria]
-    #
-    #
-    # # ## Improving readability of boolean indexing with the query method
-    #
-    # # ### How to do it...
-    #
-    # # In[77]:
-    #
-    #
-    # employee = pd.read_csv('data/employee.csv')
-    # depts = ['Houston Police Department-HPD',
-    #          'Houston Fire Department (HFD)']
-    # select_columns = ['UNIQUE_ID', 'DEPARTMENT',
-    #                   'GENDER', 'BASE_SALARY']
-    #
-    #
-    # # In[78]:
-    #
-    #
-    # qs = "DEPARTMENT in @depts "     " and GENDER == 'Female' "     " and 80000 <= BASE_SALARY <= 120000"
-    # emp_filtered = employee.query(qs)
-    # emp_filtered[select_columns].head()
-    #
-    #
-    # # ### How it works...
-    #
-    # # ### There's more...
-    #
-    # # In[79]:
-    #
-    #
-    # top10_depts = (employee.DEPARTMENT.value_counts()
-    #    .index[:10].tolist()
-    # )
-    # qs = "DEPARTMENT not in @top10_depts and GENDER == 'Female'"
-    # employee_filtered2 = employee.query(qs)
-    # employee_filtered2.head()
-    #
-    #
-    # # ## Preserving Series size with the where method
-    #
-    # # ### How to do it...
-    #
-    # # In[80]:
-    #
-    #
-    # movie = pd.read_csv('data/movie.csv', index_col='movie_title')
-    # fb_likes = movie['actor_1_facebook_likes'].dropna()
-    # fb_likes.head()
-    #
-    #
-    # # In[81]:
-    #
-    #
-    # fb_likes.describe()
-    #
-    #
-    # # In[82]:
-    #
-    #
-    # import matplotlib.pyplot as plt
-    # fig, ax = plt.subplots(figsize=(10, 8))
-    # fb_likes.hist(ax=ax)
-    # fig.savefig('/tmp/c7-hist.png', dpi=300)     # doctest: +SKIP
-    #
-    #
-    # # In[83]:
-    #
-    #
-    # criteria_high = fb_likes < 20_000
-    # criteria_high.mean().round(2)
-    #
-    #
-    # # In[84]:
-    #
-    #
-    # fb_likes.where(criteria_high).head()
-    #
-    #
-    # # In[85]:
-    #
-    #
-    # fb_likes.where(criteria_high, other=20000).head()
-    #
-    #
-    # # In[86]:
-    #
-    #
-    # criteria_low = fb_likes > 300
-    # fb_likes_cap = (fb_likes
-    #    .where(criteria_high, other=20_000)
-    #    .where(criteria_low, 300)
-    # )
-    # fb_likes_cap.head()
-    #
-    #
-    # # In[87]:
-    #
-    #
-    # len(fb_likes), len(fb_likes_cap)
-    #
-    #
-    # # In[88]:
-    #
-    #
-    # fig, ax = plt.subplots(figsize=(10, 8))
-    # fb_likes_cap.hist(ax=ax)
-    # fig.savefig('/tmp/c7-hist2.png', dpi=300)     # doctest: +SKIP
-    #
-    #
-    # # ### How it works...
-    #
-    # # ### There's more...
-    #
-    # # In[89]:
-    #
-    #
-    # fb_likes_cap2 = fb_likes.clip(lower=300, upper=20000)
-    # fb_likes_cap2.equals(fb_likes_cap)
-    #
-    #
-    # # ## Masking DataFrame rows
-    #
-    # # ### How to do it...
-    #
-    # # In[90]:
-    #
-    #
-    # movie = pd.read_csv('data/movie.csv', index_col='movie_title')
-    # c1 = movie['title_year'] >= 2010
-    # c2 = movie['title_year'].isna()
-    # criteria = c1 | c2
-    #
-    #
-    # # In[91]:
-    #
-    #
-    # movie.mask(criteria).head()
-    #
-    #
-    # # In[92]:
-    #
-    #
-    # movie_mask = (movie
-    #     .mask(criteria)
-    #     .dropna(how='all')
-    # )
-    # movie_mask.head()
-    #
-    #
-    # # In[93]:
-    #
-    #
-    # movie_boolean = movie[movie['title_year'] < 2010]
-    # movie_mask.equals(movie_boolean)
-    #
-    #
-    # # In[94]:
-    #
-    #
-    # movie_mask.shape == movie_boolean.shape
-    #
-    #
-    # # In[95]:
-    #
-    #
-    # movie_mask.dtypes == movie_boolean.dtypes
-    #
-    #
-    # # In[96]:
-    #
-    #
-    # from pandas.testing import assert_frame_equal
-    # assert_frame_equal(movie_boolean, movie_mask,
-    #        check_dtype=False)
-    #
-    #
-    # # ### How it works...
-    #
-    # # ### There's more...
-    #
-    # # In[97]:
-    #
-    #
+
+    ## Translating SQL WHERE clauses
+    employee = pd.read_csv("data/employee.csv")
+    ic(employee.dtypes)
+    ic(employee.DEPARTMENT.value_counts().head())
+    ic(employee.GENDER.value_counts())
+    ic(employee.BASE_SALARY.describe())
+    depts = ["Houston Police Department-HPD", "Houston Fire Department (HFD)"]
+    criteria_dept = employee.DEPARTMENT.isin(depts)
+    criteria_gender = employee.GENDER == "Female"
+    criteria_sal = (employee.BASE_SALARY >= 80000) & (employee.BASE_SALARY <= 120000)
+    criteria_final = criteria_dept & criteria_gender & criteria_sal
+    select_columns = ["UNIQUE_ID", "DEPARTMENT", "GENDER", "BASE_SALARY"]
+    ic(employee.loc[criteria_final, select_columns].head())
+
+    criteria_sal = employee.BASE_SALARY.between(80_000, 120_000)
+    top_5_depts = employee.DEPARTMENT.value_counts().index[:5]
+    criteria = ~employee.DEPARTMENT.isin(top_5_depts)
+    ic(employee[criteria])
+
+    ## Improving readability of boolean indexing with the query method
+    employee = pd.read_csv("data/employee.csv")
+    depts = ["Houston Police Department-HPD", "Houston Fire Department (HFD)"]
+    select_columns = ["UNIQUE_ID", "DEPARTMENT", "GENDER", "BASE_SALARY"]
+    qs = "DEPARTMENT in @depts " " and GENDER == 'Female' " " and 80000 <= BASE_SALARY <= 120000"
+    emp_filtered = employee.query(qs)
+    ic(emp_filtered[select_columns].head())
+
+    top10_depts = employee.DEPARTMENT.value_counts().index[:10].tolist()
+    qs = "DEPARTMENT not in @top10_depts and GENDER == 'Female'"
+    employee_filtered2 = employee.query(qs)
+    ic(employee_filtered2.head())
+
+    ## Preserving Series size with the where method
+    movie = pd.read_csv("data/movie.csv", index_col="movie_title")
+    fb_likes = movie["actor_1_facebook_likes"].dropna()
+    ic(fb_likes.head())
+    ic(fb_likes.describe())
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    fb_likes.hist(ax=ax)
+    fig.savefig("images/ch07/c7-hist.png", dpi=300)
+
+    criteria_high = fb_likes < 20_000
+    ic(criteria_high.mean().round(2))
+    ic(fb_likes.where(criteria_high).head())
+    ic(fb_likes.where(criteria_high, other=20000).head())
+    criteria_low = fb_likes > 300
+    fb_likes_cap = fb_likes.where(criteria_high, other=20_000).where(criteria_low, 300)
+    ic(fb_likes_cap.head())
+    ic(len(fb_likes), len(fb_likes_cap))
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    fb_likes_cap.hist(ax=ax)
+    fig.savefig("images/ch07/c7-hist2.png", dpi=300)
+
+    fb_likes_cap2 = fb_likes.clip(lower=300, upper=20000)
+    ic(fb_likes_cap2.equals(fb_likes_cap))
+
+    ## Masking DataFrame rows
+    movie = pd.read_csv("data/movie.csv", index_col="movie_title")
+    c1 = movie["title_year"] >= 2010
+    c2 = movie["title_year"].isna()
+    criteria = c1 | c2
+    ic(movie.mask(criteria).head())
+    movie_mask = movie.mask(criteria).dropna(how="all")
+    ic(movie_mask.head())
+    movie_boolean = movie[movie["title_year"] < 2010]
+    ic(movie_mask.equals(movie_boolean))
+    ic(movie_mask.shape == movie_boolean.shape)
+    ic(movie_mask.dtypes == movie_boolean.dtypes)
+
+    assert_frame_equal(movie_boolean, movie_mask, check_dtype=False)
     # get_ipython().run_line_magic('timeit', "movie.mask(criteria).dropna(how='all')")
-    #
-    #
-    # # In[98]:
-    #
-    #
     # get_ipython().run_line_magic('timeit', "movie[movie['title_year'] < 2010]")
-    #
-    #
+    ic(movie[movie["title_year"] < 2010])
+    ic(movie.mask(criteria).dropna(how="all"))
+
     # # ## Selecting with booleans, integer location, and labels
     #
     # # ### How to do it...
